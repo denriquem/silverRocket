e75f45164bf2cc65a80199f52e42a30ae6a7056c
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2022-01-18 12:27:24 +0000

    feat: deleting and updating posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -11,8 +11,13 @@ router.get("/", async (req, res) => {
 	}
 });
 
-router.get("/specific", (req, res) => {
-	res.send("specific post");
+router.get("/:postId", async (req, res) => {
+	try {
+		const post = Post.findById(req.params.postId);
+		res.json(post);
+	} catch (err) {
+		res.json({ message: err });
+	}
 });
 
 router.post("/", async (req, res) => {
@@ -21,6 +26,27 @@ router.post("/", async (req, res) => {
 	try {
 		const savedPost = await post.save();
 		res.json(savedPost);
+	} catch (err) {
+		res.json(err);
+	}
+});
+
+router.delete("/:postId", async (req, res) => {
+	try {
+		const removedPost = await Post.remove({ _id: req.params.postId });
+		res.json(removedPost);
+	} catch (err) {
+		res.json(err);
+	}
+});
+
+router.patch("/:postId", async (req, res) => {
+	try {
+		const updatedPost = await Post.updateOne(
+			{ _id: req.params.postId },
+			{ $set: { body: req.body.body } }
+		);
+		res.json(updatedPost);
 	} catch (err) {
 		res.json(err);
 	}
